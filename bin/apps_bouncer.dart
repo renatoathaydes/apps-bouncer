9bfbc0cb08039b343dc09961e218e5a27bd112ea
Renato Athaydes <renato@athaydes.com>
2024-05-05 20:06:21 +0200

    Setup logging in executable.

--- bin/apps_bouncer.dart
+++ bin/apps_bouncer.dart
@@ -1,5 +1,9 @@
 import 'package:apps_bouncer/apps_bouncer.dart' as apps_bouncer;
+import 'package:logging/logging.dart';
 
-main(List<String> arguments) async {
+main() async {
+  Logger.root.onRecord.listen((record) {
+    print('${record.time} [${record.level.name}] ${record.message}');
+  });
   await apps_bouncer.run();
 }
